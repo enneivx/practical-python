a53d6bb7e3de28faf106609447f6774c289019ac
enneivx <gcostaniera@gmail.com>
2021-01-17 02:29:22 +0800

    Completed Ex 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100 # Meters
+num_of_drops = 0
+
+while num_of_drops < 10:
+    height = round((height * 0.6), 4)
+    num_of_drops = num_of_drops + 1
+    print(num_of_drops, height)
